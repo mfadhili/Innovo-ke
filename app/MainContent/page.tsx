5969d37910c49e569b97ae811f64170eb0979708
mfadhili <mfadhili@github.com>
2024-01-28 06:03:56 +0300

    Overlay not okay

--- app/MainContent/page.tsx
+++ app/MainContent/page.tsx
@@ -9,12 +9,12 @@ function MainContent() {
                 {/*Sections*/}
                 <section className={''}>
                     {/*Hero section*/}
-                    <div className="bg-bl bg-scroll bg-cover h-[964px] bg-hero-image bg-[50%_50%] bg-blend-color-burn">
-                        <Image src={"/images/pexels-oleksandr-p-1004366.jpg"} alt={"background image"} />
+                    <div className="bg-black   h-[964px]   w-full relative">
+                        <img src={"/images/pexels-oleksandr-p-1004366.jpg"} className={"w-full h-full object-cover absolute bg-blend-overlay" }/>
                         <p className={`absolute w-[386px] top-[285px] left-[737px] ${serifFont.className} font-normal text-white text-[48px] tracking-[0] leading-[normal]`}>
                             Your Cloud Journey Starts Now
                         </p>
-                        <p className={"absolute w-[329px] top-[524px] left-[496px] font-normal text-white text-[18px] tracking-[0] leading-[30px]"}>
+                        <p className={"absolute w-[329px] top-[524px] left-[496px] font-normal text-slate-300 text-[18px] tracking-[0] leading-[30px]"}>
                             Work from anywhere, connect with everyone: Unify your communication with cloud-powered PBX.
                         </p>
 
