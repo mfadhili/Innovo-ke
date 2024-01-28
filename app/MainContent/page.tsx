8f58805927d689011642f43579cae30241c963cb
mfadhili <mfadhili@github.com>
2024-01-28 09:37:16 +0300

    hero section done

--- app/MainContent/page.tsx
+++ app/MainContent/page.tsx
@@ -1,5 +1,7 @@
 import {serifFont} from "@/app/fonts";
 import Image from "next/image";
+import {Buttons} from "@/components/Button";
+import React from "react";
 
 function MainContent() {
     return (
@@ -9,15 +11,30 @@ function MainContent() {
                 {/*Sections*/}
                 <section className={''}>
                     {/*Hero section*/}
-                    <div className="bg-bl bg-scroll bg-cover h-[964px] bg-hero-image bg-[50%_50%] bg-blend-color-burn">
-                        <Image src={"/images/pexels-oleksandr-p-1004366.jpg"} alt={"background image"} />
-                        <p className={`absolute w-[386px] top-[285px] left-[737px] ${serifFont.className} font-normal text-white text-[48px] tracking-[0] leading-[normal]`}>
-                            Your Cloud Journey Starts Now
-                        </p>
-                        <p className={"absolute w-[329px] top-[524px] left-[496px] font-normal text-white text-[18px] tracking-[0] leading-[30px]"}>
-                            Work from anywhere, connect with everyone: Unify your communication with cloud-powered PBX.
-                        </p>
-
+                    <div className="bg-bl bg-scroll bg-cover h-[1400px] bg-hero-image bg-[50%_50%] bg-blend-color-burn">
+                        <div className=" relative bg-black h-[1400px] bg-opacity-50">
+                            {/*<Image src={"/images/pexels-oleksandr-p-1004366.jpg"} alt={"background image"} />*/}
+                            <p className={`absolute w-[386px] top-[285px] left-[737px] ${serifFont.className} font-normal text-white text-[48px] tracking-[0] leading-[normal]`}>
+                                Your Cloud Journey Starts Now
+                            </p>
+                            <p className={"absolute w-[329px] top-[574px] left-[496px] font-normal text-slate-300 text-[18px] tracking-[0] leading-[30px]"}>
+                                Work from anywhere, connect with everyone: Unify your communication with cloud-powered PBX.
+                            </p>
+                            <div className="absolute w-[367px] h-[49px] top-[740px] left-[358px]">
+                                <Buttons
+                                    className="!absolute !left-0 !w-[169px] !top-0"
+                                    divClassName="!mt-[-7.50px] text-xl !mb-[-5.50px] ![white-space:unset]"
+                                    property1="default"
+                                    text="Request Demo"
+                                />
+                                <Buttons
+                                    className="!absolute !left-[203px] !w-[164px] !top-0"
+                                    divClassName="!mt-[-7.50px] text-xl !mb-[-5.50px] ![white-space:unset]"
+                                    property1="variant-4"
+                                    text="Enquire Now"
+                                />
+                            </div>
+                        </div>
                     </div>
                 </section>
                 <section className={''}>
--- public/ref/Desktop.tsx
+++ public/ref/Desktop.tsx
@@ -1,3 +1,4 @@
+ll
 import React from "react";
 import {Buttons} from "@/components/Button";
 
